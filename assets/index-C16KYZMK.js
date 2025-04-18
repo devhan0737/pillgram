f0f5cd73244a5ff1e21439045cef45f771a59666
unknown <devhsj0737@gmail.com>
2025-04-18 21:14:08 +0900

    Updates

--- assets/index-C16KYZMK.js
+++ assets/index-C16KYZMK.js
@@ -58,7 +58,7 @@ Error generating stack: `+h.message+`
       height: auto;
     }
   }
-`,Mh=({onClick:n})=>x.jsx(g0,{onClick:n,children:x.jsx("a",{href:"#",children:x.jsx("img",{src:"public/menu-line.svg",alt:""})})}),v0=[{id:1,text:"맞춤영양제 추천받기",href:"#naver"},{id:2,text:"영양제 정보",href:"#naver"},{id:3,text:"고객후기",href:"#naver"},{id:4,text:"브랜드 스토리",href:"/brand-story"},{id:5,text:"필그램 소식",href:"#naver"},{id:5,text:"가입/로그인",href:"/login",display:"none"},{id:6,text:"선물하기",href:"#naver"}],y0=Z.div`
+`,Mh=({onClick:n})=>x.jsx(g0,{onClick:n,children:x.jsx("a",{href:"#",children:x.jsx("img",{src:"${import.env.PUBLIC_URL}/menu-line.svg",alt:""})})}),v0=[{id:1,text:"맞춤영양제 추천받기",href:"#naver"},{id:2,text:"영양제 정보",href:"#naver"},{id:3,text:"고객후기",href:"#naver"},{id:4,text:"브랜드 스토리",href:"/brand-story"},{id:5,text:"필그램 소식",href:"#naver"},{id:5,text:"가입/로그인",href:"/login",display:"none"},{id:6,text:"선물하기",href:"#naver"}],y0=Z.div`
   position: absolute;
   top: 65px;
   left: 0;
